f352bf3528cd9e24ecf89e057d566401f3a963e6
joo-ai3 <y08727434@gmail.com>
2025-07-25 07:01:17 +0300

    Updated AppHeader.tsx

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -6,7 +6,8 @@ import { useCart } from '../contexts/CartContext';
 import { useFavorites } from '../contexts/FavoritesContext';
 import { useTheme } from '../contexts/ThemeContext';
 import { useLang, useTranslation } from '../contexts/LangContext';
-import Logo from './Logo';
+<img src="/logo.png" alt="Soleva Logo" className="h-10" />
+
 import MobileMenu from './MobileMenu';
 
 export default function AppHeader() {
