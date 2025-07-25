8eee6498fcb6742afc78ec5f525a51f153aa3451
joo-ai3 <y08727434@gmail.com>
2025-07-25 06:27:31 +0300

    Fix missing Link import in AppHeader

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiShoppingCart, FiUser, FiMoon, FiSun, FiHeart } from 'react-icons/fi';
 import { useAuth } from '../contexts/AuthContext';
 import { useCart } from '../contexts/CartContext';
