236bef0985d90c8bd8dccbabbdc4f7e24d3386f6
JOO-AI4 <youssefgostart@gmail.com>
2025-08-10 12:58:04 +0300

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -90,7 +90,7 @@ export default function Home() {
                 size="lg"
                 className="w-full sm:w-auto"
               >
-                {lang === 'ar' ? 'تصفح المنتجات' : 'Browse Products'}
+                {lang === 'ar' ? 'عن سوليفا' : 'About Soleva'}
                 <FiArrowRight />
               </GlassButton>
             </Link>
